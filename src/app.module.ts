0e4f2e564ab7a5ba34aca97e155ff090411eb63e
Paulo Salvatore <salvatoregames@gmail.com>
2021-01-29 12:53:45 -0300

    Endpoint 'messages' moved to an external module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './users/user.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { MessageModule } from './messages/message.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, MessageModule],
   controllers: [AppController],
   providers: [
-    AppService,
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
--- src/messages/message.service.ts
+++ src/messages/message.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { MessageDto } from './message.dto';
 
 @Injectable()
-export class AppService {
+export class MessageService {
   private messages: MessageDto[] = [];
 
   getMessages(): MessageDto[] {
